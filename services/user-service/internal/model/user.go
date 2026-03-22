f207943a6a559734a9f5584fe07a828ac550bcff
Alex Petrov <aleksispetrov777@mail.ru>
2026-03-22 14:10:41 +0300

    feat(user-service): добавлена модель LoginUser - упрощенная модель пользователя, используемая при регистрации и входе

--- services/user-service/internal/model/user.go
+++ services/user-service/internal/model/user.go
@@ -4,11 +4,18 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // User - модель пользователя
 type User struct {
-	ID primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Tag string `bson:"tag" json:"tag"`
-	Password string `bson:"password"`
-	SecretAnswer string `bson:"secret_answer"`
-	AvatarURL string `bson:"avatar_url" json:"avatar_url"`
-	Following []primitive.ObjectID `bson:"following" json:"following"`
-	FollowersCount int `bson:"followers_count" json:"followers_count"`
+	ID             primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Tag            string             `bson:"tag" json:"tag"`
+	Password       string             `bson:"password"`
+	SecretAnswer   string             `bson:"secret_answer"`
+	AvatarURL      string             `bson:"avatar_url" json:"avatar_url"`
+	FollowingCount      int                `bson:"following_count" json:"following_count"`
+	FollowersCount int                `bson:"followers_count" json:"followers_count"`
+}
+
+// LoginUser - модель пользователя для авторизации
+type LoginUser struct {
+	ID             primitive.ObjectID `bson:"_id,omitempty"`
+	Tag            string             `bson:"tag"`
+	Password       string             `bson:"password"`
 }

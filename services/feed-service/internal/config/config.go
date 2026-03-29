887d80ac70b14af327c93b9c8869b01d74a870e9
Alex Petrov <aleksispetrov777@mail.ru>
2026-03-29 20:38:38 +0300

    feat(feed-service): конфиг дополнен новыми полями

--- services/feed-service/internal/config/config.go
+++ services/feed-service/internal/config/config.go
@@ -10,21 +10,29 @@ import (
 // Config - стурктура,
 // хранящая значения по ключам env-файла
 type Config struct {
-	JWTSecret   string
+	JWTSecret      string
+	UserServiceURL string
+	PostServiceURL string
 }
 
 // LoadConfig - конструктор конфига
 // при отсутствии одного из ключа возвращает ошибку
 func LoadConfig() (*Config, error) {
-	godotenv.Load()
+	_ = godotenv.Load()
 
 	cfg := &Config{
-		JWTSecret:   os.Getenv("JWT_SECRET"),
+		JWTSecret:      os.Getenv("JWT_SECRET"),
+		UserServiceURL: os.Getenv("USER_SERVICE_URL"),
+		PostServiceURL: os.Getenv("POST_SERVICE_URL"),
 	}
 
 	switch {
 	case cfg.JWTSecret == "":
 		return nil, fmt.Errorf("JWT_SECRET is not set")
+	case cfg.UserServiceURL == "":
+		return nil, fmt.Errorf("USER_SERVICE_URL is not set")
+	case cfg.PostServiceURL == "":
+		return nil, fmt.Errorf("POST_SERVICE_URL is not set")
 	default:
 		return cfg, nil
 	}
